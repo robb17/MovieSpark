d8e1cdcfe3810e1572f6228b604b3285944e5e35
Rob Brunstad <robbrunstad@gmail.com>
2019-12-01 14:24:00 -0500

    added weights, tag relationships to the schema

--- app/models.py
+++ app/models.py
@@ -13,7 +13,12 @@ movie_cast_table = db.Table('movie_cast_table', db.Model.metadata,
 relevancy_table = db.Table('relevancy_table', db.Model.metadata,
 	db.Column('movie_id', db.Integer, ForeignKey('movie.movie_id'), index=True),
 	db.Column('referenced_movie_id', db.Integer, ForeignKey('movie.movie_id')),
+	db.Column('weight', db.Integer),
 	UniqueConstraint('movie_id', 'referenced_movie_id', name='unique_pairing'))
+
+tag_table = db.Table('tag_table', db.Model.metadata,
+	db.Column('movie_id', db.Integer, ForeignKey('movie.movie_id')),
+	db.Column('tag_id', db.Integer, ForeignKey('tag.tag_id')))
 
 class Movie(db.Model):
 	__tablename__ = 'movie'
@@ -22,6 +27,7 @@ class Movie(db.Model):
 	year_released = db.Column(db.Integer)
 	genres = relationship("Genre", secondary=movie_genre_table, back_populates="movies")
 	cast = relationship("Actor", secondary=movie_cast_table, back_populates="movies")
+	tags = relationship("Tag", secondary=tag_table, back_populates="movies")
 
 	# One-to-many relationship here is many-to-many as a result of its self-referential nature
 	weights = relationship("Movie", secondary=relevancy_table,
@@ -39,6 +45,7 @@ class Tag(db.Model):
 	__tablename__ = 'tag'
 	tag_id = db.Column(db.Integer, primary_key=True)
 	tag = db.Column(db.String(5000))
+	movies = relationship("Movie", secondary=tag_table, back_populates="tags")
 
 class Actor(db.Model):
 	__tablename__ = 'actor'
