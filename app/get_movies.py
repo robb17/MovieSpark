13c3b83a96c384adcd338f7056b1b359ec37638a
Rob Brunstad <robbrunstad@gmail.com>
2019-12-06 18:19:36 -0500

    get_movies extracts dates now

--- app/get_movies.py
+++ app/get_movies.py
@@ -1,60 +1,88 @@
 import csv
-from weights import init_weights
+#from weights import init_weights
 
 # Takes a list of pairs (movie_id, rating) and returns a list of triples with the movie name
-def get_triples(movies):
+def combine_attributes(movies):
     movie_reader = csv.reader(open('../../ml-20m/movies.csv', newline = ''), delimiter=',', quotechar = '"')
     next(movie_reader)
     movie_triples = []
-    head = 0
-    while (head < len(movies)):
-        row = next(movie_reader)
-        next_id = movies[head][0]
-        while (int(row[0]) != next_id) :
-            row = next(movie_reader)
-        movie_triples.append([next_id, row[1], movies[head][1]])
-        head += 1
+    movie_set = {}
+    for row in movie_reader:    # movie id, movie title, movie year
+        title, year = parse_title(row[1])
+        movie_set[int(row[0])] = [title, year]
+    for rated_movie in movies:
+        if movie_set.get(rated_movie[0]):
+            movie_triples.append([rated_movie[0], movie_set[rated_movie[0]][0], movie_set[rated_movie[0]][1], rated_movie[1]])
     return movie_triples
 
 # Takes a list of ratings and gets the highest rated 13779
 # Returns a list of pairs (movie_id, rating)
 def top_movies(movies):
-    rated_movies = []
-    for i in range(0, 131263):
-        if (movies[i] >= 3.25):                   # Set to 3.25 because it splits at almost 13000
-            rated_movies.append([i, movies[i]])
-    return rated_movies
+    movies_with_id = [[x, movies[x]] for x in range(1, len(movies))]
+    movies_with_id.sort(key=lambda x: x[1], reverse=True)
+    return movies_with_id[:10000]
+
+def parse_title(unparsed_title):
+    title = None
+    year = None
+    for x in range(len(unparsed_title) - 1, 0, -1):
+        if unparsed_title[x] == "(":
+            try:
+                year = int(unparsed_title[x + 1:-1].replace(")", ""))
+            except ValueError:
+                pass    # some movies dont specify year in this dataset
+            title = unparsed_title[:x - 1]
+            break
+    return (title, year)
+
+
+def n_movies(reader):
+    max_id = 0
+    for row in reader:
+        movie_id = None
+        try:
+            movie_id = int(row[1])
+        except ValueError:
+            pass
+        if movie_id:
+            max_id = movie_id if movie_id > max_id else max_id
+    return max_id + 1
 
 # Gets all rating from the ratings.csv file and gets average 0-5 rating for each movie
 # Returns a list of these ratings
 def get_ratings():
-    movie_reader = csv.reader(open('../../ml-20m/ratings.csv', newline = ''), delimiter=',', quotechar = '"')
-    ratings = [0] * 131263                   # There are 131263 total movies
-    rating_count = [0] * 131263              # Parallel arrays to track total rating for a movie,
-    count = 0                                # and number of ratings for that movie
-    for row in movie_reader:
-        if (row[0] == "userId"):             # Ignore header
-            continue
-        count += 1
-        if ((count % 1000000) == 0) :        # To see progress
-            print("ROW " + str(int(count / 1000000)) + " million")
-        movie_id = int(row[1])
-        rating_count[movie_id] += 1
-        ratings[movie_id] += float(row[2])
-    for i in range(0,131263):                # Get ratings 0-5
-        if (rating_count[i] != 0) :
-            ratings[i] = ratings[i] / rating_count[i]
-    return ratings
+    with open('../../ml-20m/ratings.csv', newline = '') as f:
+        movie_reader = csv.reader(f, delimiter=',', quotechar = '"')
+        n_m = n_movies(movie_reader)
+        f.seek(0)
+        ratings = [0] * n_m
+        rating_count = [0] * n_m
+        count = 0                                # and number of ratings for that movie
+        for row in movie_reader:
+            if (row[0] == "userId"):             # Ignore header
+                continue
+            count += 1
+            if ((count % 1000000) == 0) :        # To see progress
+                print("ROW " + str(int(count / 1000000)) + " million")
+            movie_id = int(row[1])
+            rating_count[movie_id] += 1
+            ratings[movie_id] += float(row[2])
+        for i in range(0, len(ratings)):                # Get ratings 0-5
+            if (rating_count[i] != 0) :
+                ratings[i] = ratings[i] / rating_count[i]
+        return ratings
 
-# Returns a list of the top 13799 rated movies as triples (movie_id, movie_name, rating)
+# Returns a list of the top 13799 rated movies as (movie_id, movie_name, genre, year, rating)
 def get_movies():
     all_rated_movies = get_ratings()
     top_rated_movies = top_movies(all_rated_movies)
-    top_movies_with_names = get_triples(top_rated_movies)
+    top_movies_with_names = combine_attributes(top_rated_movies)
     return top_movies_with_names
     
 top_movies = get_movies()
 print(len(top_movies))
+for i in range(9000, 9100):
+    print(top_movies[i])
 #ids = []
 #for entry in top_13799_movies :
 #    ids.append(entry[0])
