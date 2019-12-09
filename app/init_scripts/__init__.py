25f22125150c6386f97ac2377e03b9ab93b663cd
Rob Brunstad <robbrunstad@gmail.com>
2019-12-09 04:30:24 -0500

    Database too large to transmit by push

--- app/init_scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .weights import init_weights
-from .get_movies import get_movies
-from .get_genres import get_genres
-from .get_tags import get_tags_and_relevancy, get_scored_tags
--- app/init_scripts/get_genres.py
+++
@@ -1,7 +0,0 @@
-def get_genres(movies):
-    genres = []
-    for movie in movies:
-        for genre in movie[3]:
-            if genre not in genres:
-                genres.append(genre)
-    return genres
