147b2ee8f6c1eaa2f0c4b84fb33b3429516d0782
Rob Brunstad <robbrunstad@gmail.com>
2019-12-09 19:56:56 -0500

    added query processor

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import time
 
 from .init_scripts import init_weights, get_movies, get_genres, get_tags_and_relevancy, get_scored_tags
 
-from .models import Movie, Genre, Tag, TagWeight
+from .models import Movie, Genre, Tag, TagWeight, Relevance
 
 main = Blueprint('main', __name__)
 
@@ -21,16 +21,76 @@ def search(data):
 	if ((len(user_input) > 3) and (user_input[0:4] == 'the ')):
 		user_input = user_input[4:]
 	input = "%" + user_input + "%"
-	results = None
-	if query_type == 'Movie':
-		results = db.session.query(Movie).filter(Movie.name.ilike(input)).first()
-	else:
-		results = db.session.query(Tag).filter(Tag.tag.ilike(input)).first()
-	if not results:
-		return_str = "No results found"
-	else:
+	return_str, results = test_for_empty_result(query_type, input)
+	if results:
 		return_str = 'Are you thinking of <span style="cursor: pointer; text-decoration: underline; color: blue;" id="search_suggestion">' + str(results.name if query_type == "Movie" else results.tag) + '</span>?'
 	socketio.emit('title result', return_str, room=request.sid)
+
+def test_for_empty_result(query_type, final_input):
+	results = None
+	return_str = ""
+	if query_type == 'Movie':
+		results = db.session.query(Movie).filter(Movie.name.ilike(final_input)).first()
+	else:
+		results = db.session.query(Tag).filter(Tag.tag.ilike(final_input)).first()
+	if not results:
+		return_str = "No results found"
+	return (return_str, results)
+
+@socketio.on('query request')
+def find_suggestions(data):
+	user_input = data['query_text']
+	query_type = data['query_type']
+	suggestions = None
+	return_str, results = test_for_empty_result(query_type, user_input)
+	if not results:
+		socketio.emit('query result', return_str, room=request.sid)
+		return
+	if query_type == 'Movie':
+		suggestions = movie_to_suggestions(results)
+	else:
+		suggestions = tag_to_suggestions(results)
+	socketio.emit('suggestions result', suggestions, room=request.sid)
+
+def movie_to_suggestions(search_movie):
+	movies = Movie.query.all()
+	search_list = search_movie.tagweight
+	best_match_val = None
+	best_match = -1
+	count = 1
+	for movie in movies:
+		if (movie.movie_id == search_movie.movie_id):
+			continue
+		test_list = movie.tagweight
+		diff = 0
+		for i in range(0, 1128) :
+			diff += abs(search_list[i].weight - test_list[i].weight)
+		avg_diff = diff / 1128
+		diff = 0
+		for i in range(0, 1128) :
+			if (abs(search_list[i].weight - test_list[i].weight) > avg_diff) :
+				diff += abs(search_list[i].weight - test_list[i].weight)
+		diff = int((diff / 1128) * 10000)
+		relevance = db.session.query(Relevance).filter(Relevance.movie_key == search_movie.movie_id, Relevance.movie_referenced == movie.movie_id).first()
+		if not relevance:
+			relevance = db.session.query(Relevance).filter(Relevance.movie_key == movie.movie_id, Relevance.movie_referenced == search_movie.movie_id).first()
+		if relevance:
+			diff += relevance.offset
+		if best_match_val == None:
+			best_match_val = diff
+			best_match = movie.movie_id
+		if (diff < best_match_val):
+			best_match_val = diff
+			best_match = movie.movie_id
+		count += 1
+		if count % 5 == 0:
+			print("at " + str(count))
+	best_movie = db.session.query(Movie).filter(Movie.movie_id == best_match).first()
+	return_str = "You should try " + best_movie[1]
+
+def tag_to_suggestions(tag):
+	pass
+
 
 @main.route('/')
 def index():
